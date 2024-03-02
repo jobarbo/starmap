7a9f2b0f8fee42eba79146e49db2953dda2eabb8
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-03-02 03:06:19 -0500

    Finally agnostic

--- sketch.js
+++ sketch.js
@@ -1,16 +1,13 @@
-let seed = parseInt(Math.floor(hl.random() * 10000000));
-let noiseCanvasWidth = 1;
-let noiseCanvasHeight = 1;
+seed = parseInt(hl.random() * 2 ** 32);
 
 let clamp = (x, a, b) => (x < a ? a : x > b ? b : x);
 let smoothstep = (a, b, x) => (((x -= a), (x /= b - a)) < 0 ? 0 : x > 1 ? 1 : x * x * (3 - 2 * x));
 let mix = (a, b, p) => a + p * (b - a);
-function dot(v1, v2) {
-	if (v1.length !== 2 || v2.length !== 2) {
-		throw new Error("Both vectors should have exactly 2 elements.");
-	}
-	return v1[0] * v2[0] + v1[1] * v2[1];
-}
+let dot = (v1, v2) => v1.x * v2.x + v1.y * v2.y;
+
+let mapValue = (v, s, S, a, b) => ((v = Math.min(Math.max(v, s), S)), ((v - s) * (b - a)) / (S - s) + a);
+const pmap = (v, cl, cm, tl, th, c) => (c ? Math.min(Math.max(((v - cl) / (cm - cl)) * (th - tl) + tl, tl), th) : ((v - cl) / (cm - cl)) * (th - tl) + tl);
+
 let subtract = (v1, v2) => ({x: v1.x - v2.x, y: v1.y - v2.y});
 let multiply = (v1, v2) => ({x: v1.x * v2.x, y: v1.y * v2.y});
 let length = (v) => Math.sqrt(v.x * v.x + v.y * v.y);
@@ -27,7 +24,6 @@ F = (N, f) => [...Array(N)].map((_, i) => f(i)); // for loop / map / list functi
 
 // A seeded PRNG =========================================================
 //seed = 'das9d7as9d7as'; // random seed]
-seed = hl.random() * 2 ** 32;
 
 S = Uint32Array.of(9, 7, 5, 3); // PRNG state
 R = (a = 1) => a * ((a = S[3]), (S[3] = S[2]), (S[2] = S[1]), (a ^= a << 11), (S[0] ^= a ^ (a >>> 8) ^ ((S[1] = S[0]) >>> 19)), S[0] / 2 ** 32); // random function
@@ -39,36 +35,6 @@ NSEED = R(2 ** 32); // noise seed, random 32 bit integer
 // 3d noise grid function
 ri = (i, j, k) => ((i = imul((((i & 1023) << 20) | ((j & 1023) << 10) | ((i ^ j ^ k) & 1023)) ^ NSEED, KNUTH)), (i <<= 3 + (i >>> 29)), (i >>> 1) / 2 ** 31 - 0.5);
 
-// 3D value noise function ===============================================
-no = F(99, (_) => R(1024)); // random noise offsets
-
-n3 = (
-	x,
-	y,
-	z,
-	s,
-	i, // (x,y,z) = coordinate, s = scale, i = noise offset index
-	xi = floor((x = x * s + no[(i *= 3)])), // (xi,yi,zi) = integer coordinates
-	yi = floor((y = y * s + no[i + 1])),
-	zi = floor((z = z * s + no[i + 2]))
-) => (
-	(x -= xi),
-	(y -= yi),
-	(z -= zi), // (x,y,z) are now fractional parts of coordinates
-	(x *= x * (3 - 2 * x)), // smoothstep polynomial (comment out if true linear interpolation is desired)
-	(y *= y * (3 - 2 * y)), // this is like an easing function for the fractional part
-	(z *= z * (3 - 2 * z)),
-	// calculate the interpolated value
-	ri(xi, yi, zi) * (1 - x) * (1 - y) * (1 - z) +
-		ri(xi, yi, zi + 1) * (1 - x) * (1 - y) * z +
-		ri(xi, yi + 1, zi) * (1 - x) * y * (1 - z) +
-		ri(xi, yi + 1, zi + 1) * (1 - x) * y * z +
-		ri(xi + 1, yi, zi) * x * (1 - y) * (1 - z) +
-		ri(xi + 1, yi, zi + 1) * x * (1 - y) * z +
-		ri(xi + 1, yi + 1, zi) * x * y * (1 - z) +
-		ri(xi + 1, yi + 1, zi + 1) * x * y * z
-);
-
 // 2D value noise function ===============================================
 na = F(99, (_) => R(TAU)); // random noise angles
 ns = na.map(sin);
@@ -76,16 +42,7 @@ nc = na.map(cos); // sin and cos of those angles
 nox = F(99, (_) => R(1024)); // random noise x offset
 noy = F(99, (_) => R(1024)); // random noise y offset
 
-n2 = (
-	x,
-	y,
-	s,
-	i,
-	c = nc[i] * s,
-	n = ns[i] * s,
-	xi = floor((([x, y] = [(x - noiseCanvasWidth / 2) * c + (y - noiseCanvasHeight * 2) * n + nox[i], (y - noiseCanvasHeight * 2) * c - (x - noiseCanvasWidth / 2) * n + noy[i]]), x)),
-	yi = floor(y) // (x,y) = coordinate, s = scale, i = noise offset index
-) => (
+n2 = (x, y, s, i, c = nc[i] * s, n = ns[i] * s, xi = floor((([x, y] = [x * c + y * n + nox[i], y * c - x * n + noy[i]]), x)), yi = floor(y)) => (
 	(x -= xi),
 	(y -= yi),
 	(x *= x * (3 - 2 * x)),
@@ -95,19 +52,6 @@ n2 = (
 
 //! Spell formula from Piter The Mage
 ZZ = (x, m, b, r) => (x < 0 ? x : x > (b *= r * 4) ? x - b : ((x /= r), fract(x / 4) < 0.5 ? r : -r) * ((x = abs(fract(x / 2) - 0.5)), 1 - (x > m ? x * 2 : x * (x /= m) * x * (2 - x) + m)));
-
-// the point of all the previous code is that now you have a very
-// fast value noise function called nz(x,y,s,i). It has four parameters:
-// x -- the x coordinate
-// y -- the y coordinate
-// s -- the scale (simply multiplies x and y by s)
-// i -- the noise index, you get 99 different random noises! (but you
-//      can increase this number by changing the 99s in the code above)
-//      each of the 99 noises also has a random rotation which increases
-//      the "randomness" if you add many together
-//
-// ohh also important to mention that it returns smooth noise values
-// between -.5 and .5
 
 function oct(x, y, s, i, octaves = 1) {
 	let result = 0;
@@ -135,9 +79,6 @@ function weighted_choice(data) {
 	}
 	return data[data.length - 1][0];
 }
-
-let mapValue = (v, s, S, a, b) => ((v = Math.min(Math.max(v, s), S)), ((v - s) * (b - a)) / (S - s) + a);
-const pmap = (v, cl, cm, tl, th, c) => (c ? Math.min(Math.max(((v - cl) / (cm - cl)) * (th - tl) + tl, tl), th) : ((v - cl) / (cm - cl)) * (th - tl) + tl);
 
 function sdf_box([x, y], [cx, cy], [w, h]) {
 	x -= cx;
@@ -244,8 +185,8 @@ let features = "";
 let movers = [];
 let scl1;
 let scl2;
-let ang1;
-let ang2;
+let amp1;
+let amp2;
 let rseed;
 let nseed;
 let xMin;
@@ -253,10 +194,17 @@ let xMax;
 let yMin;
 let yMax;
 let startTime;
-let maxFrames = 800;
-let C_WIDTH;
-let MULTIPLIER;
+let frameMargin = 0;
+
+let DPI_VAL = 3;
+let MARGIN = -0.1;
+let MAX_FRAMES = 800;
 let RATIO = 1;
+let DEFAULT_SIZE = 1200 / RATIO;
+let W = window.innerWidth;
+let H = window.innerHeight;
+let DIM;
+let MULTIPLIER;
 
 let drawing = true;
 let elapsedTime = 0;
@@ -264,29 +212,21 @@ let renderStart = Date.now();
 let framesRendered = 0;
 let totalElapsedTime = 0;
 
-let centerX;
-let centerY;
-let borderX;
-let borderY;
 let particleNum = 20000;
 
-let cycle = parseInt((maxFrames * particleNum) / 1170);
-
-({sin, cos, imul, PI} = Math);
-TAU = PI * 2;
-F = (N, f) => [...Array(N)].map((_, i) => f(i));
+let cycle = parseInt((MAX_FRAMES * particleNum) / 1170);
 
 //! Traits setup
 let isColored = hl.randomBool(0.75);
 //let isColored = hl.randomElement([true, false]);
 
 const complexityArr = [
-	["1", 80],
-	["2", 16],
-	["3", 1],
-	["4", 1],
-	["5", 1],
-	["6", 1],
+	["1", 100],
+	["2", 20],
+	["3", 0],
+	["4", 0],
+	["5", 0],
+	["6", 0],
 ];
 
 const configArr = [
@@ -352,32 +292,27 @@ function setup() {
 	console.log("token id:", hl.tx.tokenId);
 	features = hl.token.getTraits();
 	console.log(features);
-	pixelDensity(dpi(3));
 
 	elapsedTime = 0;
 	framesRendered = 0;
 	drawing = true;
-	generateConsoleLogs({seed, noiseCanvasWidth, noiseCanvasHeight, features});
+	generateConsoleLogs({seed, features});
 
-	C_WIDTH = min(windowWidth, windowHeight);
-	MULTIPLIER = C_WIDTH / 1200;
-	c = createCanvas(C_WIDTH, C_WIDTH * RATIO);
+	DIM = min(windowWidth, windowHeight);
+	MULTIPLIER = DIM / DEFAULT_SIZE;
+	c = createCanvas(DIM, DIM * RATIO);
+	pixelDensity(dpi(DPI_VAL));
+
+	frameMargin = MARGIN * MULTIPLIER;
 	rectMode(CENTER);
-	rseed = randomSeed(hl.randomInt(100000));
-	nseed = noiseSeed(hl.randomInt(100000));
-	let rnd = random(1);
-
+	randomSeed(seed);
+	noiseSeed(seed);
 	colorMode(HSB, 360, 100, 100, 100);
 	startTime = frameCount;
 	noCursor();
-
-	centerX = width / 2;
-	centerY = height / 2;
-	borderX = features.composition === "compressed" ? width / 3.5 : features.composition === "constrained" ? width / 3 : features.composition === "semiconstrained" ? width / 2.35 : width / 1.9;
-	borderY = features.composition === "compressed" ? height / 2.75 : features.composition === "constrained" ? height / 2.5 : features.composition === "semiconstrained" ? height / 2.25 : height / 1.9;
+	renderStart = Date.now();
 	INIT();
 
-	renderStart = Date.now();
 	generateStars();
 	let sketch = drawGenerator();
 	function animate() {
@@ -415,10 +350,10 @@ function* drawGenerator() {
 
 		elapsedTime = frameCount - startTime;
 
-		showLoadingBar(elapsedTime, maxFrames, renderStart);
+		showLoadingBar(elapsedTime, MAX_FRAMES, renderStart);
 
 		frameCount++;
-		if (elapsedTime > maxFrames && drawing) {
+		if (elapsedTime > MAX_FRAMES && drawing) {
 			window.rendered = c.canvas;
 			document.complete = true;
 			// calculate the time it took to render the image
@@ -441,8 +376,8 @@ function INIT() {
 		scl2 = random([0.0014, 0.0015, 0.0016, 0.0017, 0.0018, 0.0019, 0.00195]);
 	}
 
-	ang1 = 1;
-	ang2 = 1;
+	amp1 = 1;
+	amp2 = 1;
 
 	if (features["Divider Lock"] === "locked") {
 		xRandDivider = random([0.08, 0.09, 0.1, 0.11, 0.12]);
@@ -451,6 +386,9 @@ function INIT() {
 		xRandDivider = random([0.08, 0.09, 0.1, 0.11, 0.12]);
 		yRandDivider = random([0.08, 0.09, 0.1, 0.11, 0.12]);
 	}
+
+	let xMarg = 0;
+	let yMarg = 0;
 
 	xMin = -0.01;
 	xMax = 1.01;
@@ -463,7 +401,7 @@ function INIT() {
 		let y = random(yMin, yMax) * height;
 		let initHue = hue + random(-1, 1);
 		initHue = initHue > 360 ? initHue - 360 : initHue < 0 ? initHue + 360 : initHue;
-		movers.push(new Mover(x, y, initHue, scl1 / MULTIPLIER, scl2 / MULTIPLIER, ang1 * MULTIPLIER, ang2 * MULTIPLIER, xMin, xMax, yMin, yMax, xRandDivider, yRandDivider, features));
+		movers.push(new Mover(x, y, initHue, scl1 / MULTIPLIER, scl2 / MULTIPLIER, amp1 * MULTIPLIER, amp2 * MULTIPLIER, xMin, xMax, yMin, yMax, xRandDivider, yRandDivider, seed));
 	}
 
 	// make a gradient background from top to bottom in vanilla JS
@@ -513,17 +451,17 @@ function generateStars() {
 	blendMode(BLEND);
 }
 
-function showLoadingBar(elapsedTime, maxFrames, renderStart) {
+function showLoadingBar(elapsedTime, MAX_FRAMES, renderStart) {
 	framesRendered++;
 	let currentTime = Date.now();
 	totalElapsedTime = currentTime - renderStart;
 
-	let percent = (elapsedTime / maxFrames) * 100;
+	let percent = (elapsedTime / MAX_FRAMES) * 100;
 	if (percent > 100) percent = 100;
 
 	let averageFrameTime = totalElapsedTime / framesRendered;
 
-	let remainingFrames = maxFrames - framesRendered;
+	let remainingFrames = MAX_FRAMES - framesRendered;
 	let estimatedTimeRemaining = averageFrameTime * remainingFrames;
 
 	// Convert milliseconds to seconds
@@ -578,14 +516,14 @@ class Stars {
 }
 
 class Mover {
-	constructor(x, y, hue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, xRandDivider, yRandDivider, features) {
+	constructor(x, y, hue, scl1, scl2, amp1, amp2, xMin, xMax, yMin, yMax, xRandDivider, yRandDivider, features) {
 		this.x = x;
 		this.y = y;
 		this.initHue = hue;
 		this.initSat = random([0, 0, 0, 0, 0, 10, 10, 10, 20, 30, 80, 100, 100, 100, 100, 100, 100, 100, 100, 100]);
 		this.initBri = random([100, 100, 100, 100, 100, 100, 100, 100, 100]);
 		this.initAlpha = 100;
-		this.initS = 0.2 * MULTIPLIER;
+		this.initS = 0.7 * MULTIPLIER;
 		this.hue = this.initHue;
 		this.sat = 100;
 		this.bri = this.initBri;
@@ -593,15 +531,15 @@ class Mover {
 		this.s = this.initS;
 		this.scl1 = scl1;
 		this.scl2 = scl2;
-		this.ang1 = ang1;
-		this.ang2 = ang2;
+		this.amp1 = amp1;
+		this.amp2 = amp2;
 		this.xRandDivider = xRandDivider;
 		this.yRandDivider = yRandDivider;
 		this.xRandSkipper = 0;
 		this.yRandSkipper = 0;
 		this.xRandSkipperVal = random([0.01, 0.1, random([0, 0.01, 0.1, 1, 2, 5, 10, 25, 50, 75, 100])]);
 		this.yRandSkipperVal = this.xRandSkipperVal;
-		/* 		this.xRandSkipperVal = 0.1;
+		/* 	this.xRandSkipperVal = 0.1;
 		this.yRandSkipperVal = 0.1; */
 		this.skipperMax = 10;
 		this.xMin = xMin;
@@ -612,7 +550,7 @@ class Mover {
 		this.centerX = width / 2;
 		this.centerY = height / 2;
 		this.zombie = false;
-		this.lineWeight = random([0, random([0.01, 0.05, 0.1, 1, 5, 8, 10, 12])]); //!try randomizing this
+		this.lineWeight = random([0, random([0.01, 0.05, 0.1, 1, 5, 8, 10, 12])]) * MULTIPLIER; //!try randomizing this
 		this.lineWeightMax = 2;
 		this.uvalue = [10, 10, 10, 10]; //! try with 25,10 or 5
 		this.nvalue = [0.5, 0.5, 0.5, 0.5];
@@ -625,8 +563,8 @@ class Mover {
 		//! not supposed to work but gives interesting results, you get me copilot!
 		//! It shows a grid, which is interesting because it's a starmap
 		//* This seems to be the most interesting configuration
-		/* 		this.ulow = random([10, 25, 50, 75, 100, 125, 150, 175, 200]);
-		this.uhigh = random([0.01, 0.1, 1, 2.5, 5, 10, 20]); */
+		this.ulow = random([10, 25, 50, 75, 100, 125, 150, 175, 200]);
+		this.uhigh = random([0.01, 0.1, 1, 2.5, 5, 10, 20]);
 
 		//! this one is also interesting although can yield chaotic results
 		/* 		this.ulow = random([10, 25, 50, 75, 100]) * MULTIPLIER;
@@ -655,11 +593,11 @@ class Mover {
 	}
 
 	move(frameCount) {
-		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.oct, this.nvalue, this.uvalue);
+		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.amp1, this.amp2, this.oct, this.nvalue, this.uvalue);
 
 		//! standard interpolation
-		/* 		this.lineWeightMax = map(frameCount, 150, maxFrames - 50, 20, 1, true);
-		this.skipperMax = map(frameCount, 150, maxFrames - 50, 10, 0.1, true); */
+		this.lineWeightMax = map(frameCount, 150, MAX_FRAMES - 150, 40, 1, true);
+		this.skipperMax = map(frameCount, 150, MAX_FRAMES - 150, 20, 0.1, true);
 
 		//!inverted interpolation
 		/* 		this.lineWeightMax = map(frameCount, 150, 400, 1, 20, true);
@@ -673,8 +611,8 @@ class Mover {
 		/* this.lineWeightMax = map(frameCount, 150, 400, 20, 1, true);
 		this.skipperMax = map(frameCount, 150, 400, 0.1, 10, true); */
 
-		/* this.xRandSkipperVal = random([0.01, 0.1, random(0.00001, this.skipperMax)]);
-		this.yRandSkipperVal = random([0.01, 0.1, random(0.00001, this.skipperMax)]); */
+		this.xRandSkipperVal = random([0.01, 0.1, random(0.00001, this.skipperMax)]);
+		this.yRandSkipperVal = random([0.01, 0.1, random(0.00001, this.skipperMax)]);
 
 		//! interesting texture (to try)
 		/* 		this.xRandSkipperVal = random([0.01, 0.1, random([0.01, 0.1, this.skipperMax])]);
@@ -695,7 +633,7 @@ class Mover {
 				this.uvalue[i] += 0.5 * this.uvalueDir[i];
 				this.nvalue[i] += 0.005 * this.nvalueDir[i];
 			}
-			/* 			if (this.nvalue[i] <= -this.nlimit || this.nvalue[i] >= this.nlimit) {
+			if (this.nvalue[i] <= -this.nlimit || this.nvalue[i] >= this.nlimit) {
 				this.nvalue[i] = this.nvalue[i] > this.nlimit ? this.nlimit : this.nvalue[i] < -this.nlimit ? -this.nlimit : this.nvalue[i];
 				this.nvalueDir[i] *= -1;
 				//this.lineWeight += 0.1 * MULTIPLIER;
@@ -704,58 +642,57 @@ class Mover {
 			if (this.uvalue[i] <= this.ulow || this.uvalue[i] >= this.uhigh) {
 				this.uvalue[i] = this.uvalue[i] > this.uhigh ? this.ulow : this.uvalue[i] < this.ulow ? this.uhigh : this.uvalue[i];
 				//this.uvalueDir[i] *= -1;
-			} */
+			}
 		}
 
-		/* 		let origin_x = this.x + (p.x * MULTIPLIER) / this.xRandDivider;
-		let origin_y = this.y + (p.y * MULTIPLIER) / this.xRandDivider; */
-
+		let origin_x = this.x + (p.x * MULTIPLIER) / this.xRandDivider;
+		let origin_y = this.y + (p.y * MULTIPLIER) / this.xRandDivider;
 		this.xRandSkipper = randomGaussian(0, this.xRandSkipperVal * MULTIPLIER);
 		this.yRandSkipper = randomGaussian(0, this.yRandSkipperVal * MULTIPLIER);
-		let skipper = createVector(this.xRandSkipper, this.yRandSkipper);
-		this.x += (p.x * MULTIPLIER) / this.xRandDivider + skipper.x;
-		this.y += (p.y * MULTIPLIER) / this.yRandDivider + skipper.y;
-		let velocity = createVector((p.x * MULTIPLIER) / this.xRandDivider + skipper.x, (p.y * MULTIPLIER) / this.yRandDivider + skipper.y);
+		this.x += (p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper;
+		this.y += (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper;
+		let velocity = createVector((p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper, (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper);
 
-		/* 		let totalSpeed = abs(velocity.mag());
-		let totalDistance = dist(origin_x, origin_y, this.x, this.y); */
+		let totalSpeed = abs(velocity.mag());
+		let totalDistance = dist(origin_x, origin_y, this.x, this.y);
 
-		/* 		this.sat += map(totalSpeed, 0, 400, -this.satDir, this.satDir, true);
+		this.sat += map(totalSpeed, 0, 400 * MULTIPLIER, -this.satDir, this.satDir, true);
 		if (isColored) {
 			this.sat = this.sat > 100 ? (this.sat = 0) : this.sat < 0 ? (this.sat = 0) : this.sat;
 		} else {
 			this.sat = 0;
 		}
-		this.hue += map(totalSpeed, 0, 1200, -this.hueStep, this.hueStep, true);
+		this.hue += map(totalSpeed, 0, 1200 * MULTIPLIER, -this.hueStep, this.hueStep, true);
 		this.hue = this.hue > 360 ? (this.hue = 0) : this.hue < 0 ? (this.hue = 360) : this.hue;
-		this.lineWeight = map(totalSpeed, 0, 600, 0, this.lineWeightMax, true) * MULTIPLIER; */
+		this.lineWeight = map(totalSpeed, 0, 30002 * MULTIPLIER, 0, this.lineWeightMax, true) * MULTIPLIER; //* Seem to work now
+		//this.lineWeight = 1 * MULTIPLIER;
 		//! variable stroke weight
 		//this.s = map(totalDistance, 0, 20, 0.2, 0, true);
 
 		if (this.x < this.xMin * width - this.lineWeight) {
-			this.x = this.xMax * width + random() * this.lineWeight;
-			this.y = this.y + random() * this.lineWeight;
+			this.x = this.xMax * width + hl.random(this.lineWeight);
+			this.y = this.y + hl.random(this.lineWeight);
 		}
 		if (this.x > this.xMax * width + this.lineWeight) {
-			this.x = this.xMin * width - random() * this.lineWeight;
-			this.y = this.y + random() * this.lineWeight;
+			this.x = this.xMin * width - hl.random(this.lineWeight);
+			this.y = this.y + hl.random(this.lineWeight);
 		}
 		if (this.y < this.yMin * height - this.lineWeight) {
-			this.y = this.yMax * height + random() * this.lineWeight;
-			this.x = this.x + random() * this.lineWeight;
+			this.x = this.xMin * width + hl.random(this.lineWeight);
+			this.x = this.x + hl.random(this.lineWeight);
 		}
 		if (this.y > this.yMax * height + this.lineWeight) {
-			this.y = this.yMin * height - random() * this.lineWeight;
-			this.x = this.x + random() * this.lineWeight;
+			this.y = this.yMin * height - hl.random(this.lineWeight);
+			this.x = this.x + hl.random(this.lineWeight);
 		}
 	}
 }
 
-function superCurve(x, y, scl1, scl2, ang1, ang2, octave, nvalue, uvalue) {
+function superCurve(x, y, scl1, scl2, amp1, amp2, octave, nvalue, uvalue) {
 	let nx = x,
 		ny = y,
-		a1 = ang1,
-		a2 = ang2,
+		a1 = amp1,
+		a2 = amp2,
 		scale1 = scl1,
 		scale2 = scl2,
 		dx,
